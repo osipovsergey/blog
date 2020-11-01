81c484bf761aef865234e99847271b5fddca1902
Osipov Sergey <osipovsergei97@gmail.com>
2020-11-01 13:20:49 +0300

    Add stylelint config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-// Test import of styles
-import './less/main.less';
-
-import Vue from 'vue';
-import App from './components/index.vue';
-
-new Vue({
-    el: '#app',
-    render: h => h(App)
-});
+// Import of styles
+import './less/styles/main.less';

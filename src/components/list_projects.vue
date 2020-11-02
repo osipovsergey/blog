5af2964d781cbb78bb11ee7705d397ee993e0c71
Osipov Sergey <osipovsergei97@gmail.com>
2020-11-02 20:41:30 +0300

    Add components for list_projects

---
+++ src/components/list_projects.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="project">
+        <div :key="item.id"
+            v-for="item in arrayProjects"
+            class="project__wrapper"
+        >
+            <!-- <img src="../images/main__image.jpg" alt="main__image" class="project__image"> -->
+            <div class="project__content-text">
+                <span class="project__title">{{ item.title }}</span>
+                <span class="project__desc">{{ item.desc }}</span>
+                <a v-bind:href=item.link class="project__desc">Ссылка</a>
+            </div>
+        </div>
+	</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            arrayProjects: [
+                {
+                    title: 'Default Template',
+                    desc: 'Шаблон для начала работы над своим проектом. Webpack 5 + Vue 3 + Less + JEST + PostCSS',
+                    link: 'https://github.com/osipovsergey/default_template'
+                },
+                {
+                    title: 'Default Template',
+                    desc: 'Шаблон для начала работы над своим проектом. Webpack 5 + Vue 3 + Less + JEST + PostCSS',
+                    link: 'https://github.com/osipovsergey/default_template'
+                },
+            ],
+        }
+    }
+}
+</script>
+
+<style lang="less">
+@import '../less/mixins/mixins.less';
+
+</style>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // Import of styles
 import './less/styles/main.less';
+
+import Vue from 'vue';
+import ListProjects from './components/list_projects.vue';
+
+new Vue({
+    el: '#list_projects',
+    render: h => h(ListProjects)
+});
